982312910f4135a63f44d61265a855982a5af334
PankovAlex <pankov92@mail.ru>
2021-11-14 16:45:09 +0400

    Add RandomString

--- dotnetdev.homework.7/src/WebClient/CustomerRequests.cs
+++ dotnetdev.homework.7/src/WebClient/CustomerRequests.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace WebClient
         }
         internal static async Task<Customer> CreateRundomCustomerAsync()
 		{
-            return new Customer { Firstname = "Панков", Lastname = "Александр"};
+            return new Customer { Firstname = RandomString(10), Lastname = RandomString(10)};
         }
 
 		internal static async Task<long> AddCustomerAsync(Customer customer)
@@ -30,5 +31,12 @@ namespace WebClient
             }            
             return -1;
         }
-	}
+        private static string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
 }
