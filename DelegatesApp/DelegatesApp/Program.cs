ee827d4751ba34ed4938caa551da642fc83242be
PankovAlex <pankov92@mail.ru>
2021-12-14 18:05:19 +0400

    Added readme

--- DelegatesApp/DelegatesApp/Program.cs
+++ DelegatesApp/DelegatesApp/Program.cs
@@ -10,15 +10,15 @@ namespace DelegatesApp
 	{
 		public static void Main(string[] args)
 		{
-			var myClasses = new List<MyClass>
+			var test1 = new List<MyClass>
 			{
 				{ new MyClass(741) },
 				{ new MyClass(2) },
 				{ new MyClass(3) },
 				{ new MyClass(4) },
-			};
-			var res = myClasses.GetMax<MyClass>(item => item.Value);
-			var res2 = new List<int> { 1, 22, 12, 23 }.GetMax<int>(item => item);
+			}.GetMax<MyClass>(item => item.Value);
+
+			var test2 = new List<int> { 1, 22, 12, 23 }.GetMax<int>(item => item);
 		}
 	}
 	public class MyClass
