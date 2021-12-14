a3d6d22c80c2a881c398d9bd65e2c0ceb3d7942d
PankovAlex <pankov92@mail.ru>
2021-12-14 17:19:54 +0400

    Create DelegatesApp

---
+++ DelegatesApp/DelegatesApp/MyExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+	public static class MyExtensions
+	{
+		public static T GetMax<T>(this IEnumerable e, Func<T, float> getParameter) where T : class
+		{
+
+			float maxValue = getParameter((T)e.GetEnumerator().Current);
+			T resultItem = (T)e.GetEnumerator();
+			foreach (var item in e)
+			{
+				var targetValue = getParameter((T)item);
+				if (targetValue > maxValue)
+				{
+					resultItem = (T)e;
+				}
+			}
+			return resultItem;
+		}
+	}
+}
---
+++ DelegatesApp/DelegatesApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExtensionMethods;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var q = new List<int>();
+			q.GetMax<MyClass>(z => z.MyProperty);
+		}
+	}
+	public class MyClass
+	{
+		public float MyProperty { get; set; }
+	}
+}
