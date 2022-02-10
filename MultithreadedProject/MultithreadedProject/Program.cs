c8046e9d5a4ddc652081b1aa7a00f10535d809f4
PankovAlex <pankov92@mail.ru>
2022-02-10 17:29:00 +0400

    Edit СalculateSumInMultiThread

--- MultithreadedProject/MultithreadedProject/Program.cs
+++ MultithreadedProject/MultithreadedProject/Program.cs
@@ -10,7 +10,7 @@ namespace MultithreadedProject
 	class Program
 	{
 		private static object lockObj = new object();
-		private static int listCountElements = 10000000;
+		private static int listCountElements = 100000000;
 		static void Main(string[] args)
 		{
 			List<int> list = new List<int>();
@@ -18,15 +18,15 @@ namespace MultithreadedProject
 			Random random = new Random();
 			for (int i = 0; i < listCountElements; i++)
 			{
-				list.Add(random.Next(-100, 101));
+				list.Add(random.Next(-1000, 1001));
 			}
-			calculateSumInSingleThread(list);
-			calculateSumInMultiThread(list);
-			calculateSumInParallelLinq(list);
+			СalculateSumInSingleThread(list);
+			СalculateSumInMultiThread(list);
+			СalculateSumInParallelLinq(list);
 			Console.ReadKey();
 		}
 
-		private static void calculateSumInParallelLinq(List<int> list)
+		private static void СalculateSumInParallelLinq(List<int> list)
 		{
 			Stopwatch stopWatch = new Stopwatch();
 			long sumResult = 0;
@@ -36,31 +36,31 @@ namespace MultithreadedProject
 			Console.WriteLine($"Сумма {listCountElements} элементов = {sumResult}.Вычисление в ParallelLinq. Затраченное время в тактах таймера = {stopWatch.ElapsedTicks}");
 		}
 
-		private static object qwe(int n)
-		{
-			throw new NotImplementedException();
-		}
-
-		private static void calculateSumInMultiThread(List<int> list)
+		private static void СalculateSumInMultiThread(List<int> list)
 		{
 			Stopwatch stopWatch = new Stopwatch();
+			var countTasks = 10000;
 			long sumResult = 0;
+			List<Task<int>> tasks = new List<Task<int>>();
+			var arrays = list.ToArray().Split<int>(countTasks);
 			stopWatch.Start();
-			foreach (var item in list)
+			foreach (var item in arrays)
 			{
-				Task.Run(() =>
+				tasks.Add(Task.Run(() =>
 				{
-					lock (lockObj)
-					{
-						sumResult += item;
-					}
-				});
+					return  item.Sum();
+				}));
+			}
+			Task.WaitAll(tasks.ToArray());
+			foreach (var item in tasks)
+			{
+				sumResult += item.Result;
 			}
 			stopWatch.Stop();
 			Console.WriteLine($"Сумма {listCountElements} элементов = {sumResult}.Многопоточное вычесление. Затраченное время в тактах таймера = {stopWatch.ElapsedTicks}");
 		}
 
-		private static long calculateSumInSingleThread(List<int> list)
+		private static long СalculateSumInSingleThread(List<int> list)
 		{
 			Stopwatch stopWatch = new Stopwatch();
 			long sumResult = 0;
